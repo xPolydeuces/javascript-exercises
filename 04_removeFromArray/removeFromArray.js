8483d380c6cfe808c45e9a653ac8fae744b25402
Polydeuces <bartosz.kot.00@gmail.com>
2022-07-07 19:32:55 +0200

    Solve removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const oldArray = args[0];
+    let newArray = [];
+    for (let i = 0; oldArray.length > i; i++){
+        if (!args.includes(oldArray[i])){
+            newArray.push(oldArray[i])
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
