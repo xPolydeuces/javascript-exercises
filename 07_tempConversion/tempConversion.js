f362ad5ceb88804513c45fffb6c7e7f240ca82f5
Polydeuces <bartosz.kot.00@gmail.com>
2022-07-07 20:20:13 +0200

    Solve tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempInF) {
+    const tempInC = (tempInF - 32) * (5/9);
+    return Math.round((tempInC + Number.EPSILON) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(tempInC) {
+    const tempInF = tempInC * (9/5) + 32;
+    return Math.round((tempInF + Number.EPSILON) * 10) / 10;
 };
 
 // Do not edit below this line
