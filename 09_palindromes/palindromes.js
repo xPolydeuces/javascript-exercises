35930defb33c8d7f74192aa13467ac456381e3b9
Polydeuces <bartosz.kot.00@gmail.com>
2022-07-07 20:50:53 +0200

    Solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const strToArr = str.toLowerCase().split('');
+    let newArr = [];
+    for (let i = 0; strToArr.length > i; i++){
+        if (![' ', ',', '.', '!'].includes(strToArr[i])){
+            newArr.push(strToArr[i]);
+        }
+    }
+    const arrToStr = newArr.join('');
+    const reversedStr = newArr.reverse().join('');
+    return arrToStr === reversedStr;
 };
 
 // Do not edit below this line
