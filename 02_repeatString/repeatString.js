ea3c1ed1666f74dd83fad5bb04e7cb586c2e02e5
Polydeuces <bartosz.kot.00@gmail.com>
2022-07-07 11:39:07 +0200

    Solve repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeats) {
+    if (repeats < 0) return 'ERROR';
+    return str.repeat(repeats);
 };
 
 // Do not edit below this line
