83cb806d71a9590005d9f7436130e0e7be3879d3
Polydeuces <bartosz.kot.00@gmail.com>
2022-07-07 20:39:41 +0200

    Solve calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(...nums) {
+    return nums[0] + nums[1]
 };
 
-const subtract = function() {
-	
+const subtract = function(...nums) {
+    return nums[0] - nums[1]
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    let arrSum = 0;
+    for (let i = 0; arr.length > i; i++){
+        arrSum += arr[i];
+    }
+    return arrSum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    let arrSum = 1;
+    for (let i = 0; arr.length > i; i++){
+        arrSum *= arr[i];
+    }
+    return arrSum;
 };
 
-const power = function() {
-	
+const power = function(...nums) {
+    return nums[0] ** nums[1];
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    if (num == 0) return 1;
+    let arrSum = 1;
+    const rangeArray =  Array.from({ length: num + 1}, (_, i) => i);
+    for (let i = 1; rangeArray.length > i; i++){
+        arrSum *= rangeArray[i];
+    }
+    return arrSum;
 };
 
 // Do not edit below this line
