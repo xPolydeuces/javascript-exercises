6dbab7f6f6c79e653691af7881bb720e7032c398
Polydeuces <bartosz.kot.00@gmail.com>
2022-07-07 19:57:37 +0200

    Solve sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, stop) {
+    for (let i = 0; arguments.length > i; i++){
+        if (typeof arguments[i] != "number" || arguments[i] < 0){
+            return "ERROR";
+        }
+    }
+    if(start > stop){
+        [start, stop] = [stop, start];
+    }
+    const rangeArray =  Array.from({ length: (stop - start) / 1 + 1}, (_, i) => start + i);
+    let sum = 0;
+    for (let i = 0; rangeArray.length > i; i++){
+        sum+= rangeArray[i];
+    }
+    return sum;
 };
 
 // Do not edit below this line
