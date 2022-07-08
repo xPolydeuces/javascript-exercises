5617fab88aff4c942d071e28eef6cd4e72fe59bf
Polydeuces <bartosz.kot.00@gmail.com>
2022-07-08 22:10:39 +0200

    Solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (typeof num == "string"){
+        num = parseInt(num);
+    }
+    if(num < 0) return "OOPS";
+    let num1 = 1, num2 = 1;
+    let temp;
+    for (let i = 1; num > i; i++){
+        temp = num1 + num2;
+        num1 = num2;
+        num2 = temp;
+    }
+    return num1;
 };
+
+console.log(fibonacci(15))
 
 // Do not edit below this line
 module.exports = fibonacci;
