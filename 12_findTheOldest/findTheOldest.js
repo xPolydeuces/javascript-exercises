24fe57b059956dd05bc367e7cd0c4fa5bf85cf86
Polydeuces <bartosz.kot.00@gmail.com>
2022-07-10 15:12:38 +0200

    Solve findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    let highestAge = 0;
+    let oldestPerson;
+    for (let i = 0; people.length > i; i++){
+        if (people[i].hasOwnProperty('yearOfDeath')){
+            personAge = people[i]['yearOfDeath'] - people[i]['yearOfBirth'];
+        } else {
+            personAge = currentYear - people[i]['yearOfBirth'];
+        }
+        if (personAge > highestAge){
+            highestAge = personAge;
+            oldestPerson = people[i];
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
